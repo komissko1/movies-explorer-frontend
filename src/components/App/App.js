095b28a9fa6f200d7288e497502d3cf087503daa
komissko1 <komissko1@yandex.ru>
2022-03-30 12:58:07 +0300

    Ready environment

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+
+import './App.css';
+
+function App() {
+}
+
+export default App;
